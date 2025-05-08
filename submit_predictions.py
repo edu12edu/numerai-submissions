4c3bcaf0b4208c91154b67cdd2e3b58a48b9ef41
edu12edu <bropasadbg@gmail.com>
2025-05-08 02:24:02 +0200

    mensaje

---
+++ submit_predictions.py
@@ -0,0 +1,11 @@
+import os
+from numerapi import NumerAPI
+
+public_id = os.getenv("NUMERAI_PUBLIC_ID")
+secret_key = os.getenv("NUMERAI_SECRET_KEY")
+model_id = os.getenv("NUMERAI_MODEL_ID")
+
+napi = NumerAPI(public_id=public_id, secret_key=secret_key)
+submission_file = "submission.csv"
+napi.upload_predictions(submission_file, model_id=model_id)
+
